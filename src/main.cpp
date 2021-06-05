4ca31d17d27973431f93a58a89990a0b8f223db7
Fiammante95 <43271881+Fiammante95@users.noreply.github.com>
2021-06-05 10:39:09 +0200

    fetch\pull example

--- src/main.cpp
+++ src/main.cpp
@@ -58,7 +58,7 @@ void setup() {
   // initialize the LCD
   lcd.init();
 
-  // Turn on the blacklight and print a message.
+  // Turn on the blacklight and print a message..
   lcd.backlight();
 
   lcd.setCursor(0, 0);
